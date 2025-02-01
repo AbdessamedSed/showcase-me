dfd2596e5219d4a6f79e5d058adbc37fe5743ea2
Abdessamed <ka_seddiki@esi.dz>
2025-02-01 16:09:03 +0100

    some modifications

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import { FaXTwitter } from "react-icons/fa6";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-1000 text-white py-6 px-4">
+    <footer className="bg-gray-1000 dark:text-white text-black py-6 px-4">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         {/* Left Section: Contact Info */}
         <div className="flex flex-col space-y-2 text-sm">
@@ -28,25 +28,25 @@ export default function Footer() {
 
         {/* Center Section: Social Media Icons */}
         <div className="flex space-x-4 mt-4 md:mt-0">
-          <a href="https://www.linkedin.com/in/abdessamed-seddiki-15019b211/" className="text-white hover:text-blue-400 text-xl">
+          <a href="https://www.linkedin.com/in/abdessamed-seddiki-15019b211/" className="dark:text-white text-black hover:text-blue-400 text-xl">
             <FaLinkedin />
           </a>
-          <a href="https://scholar.google.com/citations?user=K86_jrkAAAAJ&hl=fr" className="text-white hover:text-blue-400 text-xl">
+          <a href="https://scholar.google.com/citations?user=K86_jrkAAAAJ&hl=fr" className="dark:text-white text-black hover:text-blue-400 text-xl">
             <SiGooglescholar />
           </a>
-          <a href="https://www.researchgate.net/profile/Abdessamed-Seddiki?ev=hdr_xprf" className="text-white hover:text-blue-400 text-xl">
+          <a href="https://www.researchgate.net/profile/Abdessamed-Seddiki?ev=hdr_xprf" className="dark:text-white text-black hover:text-blue-400 text-xl">
             <SiResearchgate />
           </a>
-          <a href="https://github.com/AbdessamedSed" className="text-white hover:text-blue-400 text-xl">
+          <a href="https://github.com/AbdessamedSed" className="dark:text-white text-black hover:text-blue-400 text-xl">
             <FaGithub />
           </a>
-          <a href="https://x.com/Abdou28710" className="text-white hover:text-blue-400 text-xl">
+          <a href="https://x.com/Abdou28710" className="dark:text-white text-black hover:text-blue-400 text-xl">
             <FaXTwitter />
           </a>
-          <a href="https://www.facebook.com/abdessamed.seddiki.75/" className="text-white hover:text-blue-400 text-xl">
+          <a href="https://www.facebook.com/abdessamed.seddiki.75/" className="dark:text-white text-black hover:text-blue-400 text-xl">
             <FaFacebook />
           </a>
-          <a href="https://instagram.com" className="text-white hover:text-blue-400 text-xl">
+          <a href="https://instagram.com" className="dark:text-white text-black hover:text-blue-400 text-xl">
             <FaInstagram />
           </a>
         </div>
